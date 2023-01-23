8ea778a9e4feda93dd4bf85ff70e5e22cfbed734
MDkamrulJaman <mdkamruljamanjk@gmail.com>
2023-01-23 11:43:03 +0600

    change

---
+++ setInervalError.js
@@ -0,0 +1,22 @@
+temp = 0;
+let names = ["abu rayhan", "sakar hossain", "akib ahmed", "alvi chowdhuri"];
+
+console.log(typeof names);
+let index = 0;
+
+let name = names[index];
+a = setInterval(() => {
+  let name = names[index++];
+  let names = ["abu rayhan", "sakar hossain", "akib ahmed", "alvi chowdhuri"];
+  console.log(name, name.length); //we will get  abu rayhan 10 than it will give error.
+  names = ""; // It will give a error because of taking empty string.
+
+  if (index == names.length) {
+    index = 0;
+    temp++;
+  }
+
+  if (temp === 2) {
+    clearInterval(a);
+  }
+}, 1000);
